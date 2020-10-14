155097e25d575b751d9696847fafe1325d3f1ce2
fabbud <fabbud@gmail.com>
2020-10-14 20:40:37 +0100

    ideias para homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -4,12 +4,27 @@ import './Homepage.css';
 function Homepage() {
   return (
     <div className="flex-homepage">
-      <h1 className="titulo-livro">Como Desaparecer Completamente</h1>
-      <img
-        className="capa-livro"
-        src={require('../assets/capa_livro.png')}
-        alt="capa do livro"
-      />
+      <div className="div-img">
+        <img
+          className="capa-livro"
+          src={require('../assets/capa_livro.png')}
+          alt="capa do livro"
+        />
+      </div>
+      <div className="div-text">
+        <h1 className="titulo-livro">Como Desaparecer Completamente</h1>
+        <p className="desc-livro">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+          libero sapien, mattis eget purus vel, viverra suscipit dui. In vel
+          vulputate mi. Vivamus auctor neque vitae ligula porttitor, sit amet
+          tristique augue auctor. Ut vel tortor non velit dapibus ultricies.
+          Nullam porta consectetur augue at sodales. Vivamus vel neque ac tortor
+          finibus tempor. Suspendisse potenti. Proin convallis risus eget eros
+          tempus, vel scelerisque orci sagittis. Nam accumsan neque a leo ornare
+          pretium.
+        </p>
+        <button className="compre">Compre por R$30</button>
+      </div>
     </div>
   );
 }
